4b79ab4000444df7a500e66234043fea4c2bfc04
GraftonBorge <graftonmborge@gmail.com>
2021-03-22 14:51:32 -0700

    add basic calculator functions with user input
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+
+
+function subtract(number3, number4) {
+  return number3 - number4;
+}
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+alert(subtract(number3, number4));
+
+
+function multiply(number5, number6) {
+  return number5 * number6;
+}
+const number5 = parseInt(prompt("Enter a number:"));
+const number6 = parseInt(prompt("Enter another number:"));
+alert(multiply(number5, number6));
+
+
+function divide(number7, number8) {
+  return number7 / number8;
+}
+const number7 = parseInt(prompt("Enter a number:"));
+const number8 = parseInt(prompt("Enter another number:"));
+alert(divide(number7, number8));
